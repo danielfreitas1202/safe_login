a9024b8881972a1949292ed5f9eafd5f47d7d804
danielfreitas1202 <danielof1202@gmail.com>
2026-04-29 16:04:35 -0300

    adicionado validação de email

--- login.php
+++ login.php
@@ -8,9 +8,20 @@ if (!isset($_POST['email']) || !isset($_POST['senha'])) {
     exit;
 }
 
-$email = $_POST['email'];
-$senha = md5($_POST['senha']);
+$email = trim($_POST['email']);
+$senha = trim(md5($_POST['senha'])); //Muda o tipo de criptografia
 $ip = $_SERVER['REMOTE_ADDR'];
+
+    // verifica se há campos vazios
+if (empty($email) || empty($senha)) {
+    echo "Preencha todos os campos";
+    exit; // interrompe o script
+}
+    // valida formato do email
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo "Email inválido";
+    exit;
+}
 
 // Busca usuário (CORRIGIDO)
 $sql = "SELECT id_usuario, nivel, senha_hash, ativo 
@@ -18,6 +29,13 @@ $sql = "SELECT id_usuario, nivel, senha_hash, ativo
         WHERE email = '$email'";
 
 $result = mysqli_query($conn, $sql);
+
+//não tem nenhuma validação de entrada
+
+
+//não tem limite de tentativas de login 
+
+//não armazena dados de quem tenta fazer login
 
 if (mysqli_num_rows($result) > 0) {
     
