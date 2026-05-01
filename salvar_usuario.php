eac5d4f7b9f44f7b20fcd76c2ef20e2295d9114c
danielfreitas1202 <danielof1202@gmail.com>
2026-05-01 16:50:35 -0300

    Adicionado proteção contra SQL Injection.

--- salvar_usuario.php
+++ salvar_usuario.php
@@ -13,15 +13,23 @@ $senha = password_hash($_POST['senha'], PASSWORD_DEFAULT);
 $nivel = $_POST['nivel'];
 $ativo = $_POST['ativo'];
 
-$sql = "INSERT INTO usuario (nome, email, senha_hash, nivel, ativo, data_criacao) 
-        VALUES ('$nome', '$email', '$senha', '$nivel', '$ativo', NOW())";
+$stmt = mysqli_prepare($conn, 
+    "INSERT INTO usuario (nome, email, senha_hash, nivel, ativo, data_criacao) 
+     VALUES (?, ?, ?, ?, ?, NOW())"
+);
 
-$result = mysqli_query($conn, $sql);
+mysqli_stmt_bind_param($stmt, "ssssi", $nome, $email, $senha, $nivel, $ativo);
+
+$result = mysqli_stmt_execute($stmt);
 
 if ($result) {
     echo "Usuário cadastrado com sucesso!<br>"; 
     echo "<a href='listar_usuarios.php'>Ver usuários</a>";
 } else {
-    echo "Erro: " . mysqli_error($conn);
+    if (mysqli_errno($conn) == 1062) {
+        echo "Email já cadastrado.";
+    } else {
+        echo "Erro: " . mysqli_error($conn);
+    }
 }
 ?>
